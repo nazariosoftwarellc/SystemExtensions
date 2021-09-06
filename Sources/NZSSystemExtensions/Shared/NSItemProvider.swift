33789ad563f98ae9287bae8d9c52da5679a4cb49
kyle-n <kylebnazario@gmail.com>
2021-09-06 16:49:08 -0400

    General min version, SFCBM

--- Sources/NZSSystemExtensions/Shared/NSItemProvider.swift
+++ Sources/NZSSystemExtensions/Shared/NSItemProvider.swift
@@ -9,7 +9,6 @@ import Combine
 import Foundation
 
 public extension NSItemProvider {
-    @available(macOS 10.15, iOS 13.0, *)
     func loadItem(
         forTypeIdentifier identifier: String,
         options: [AnyHashable: Any]?
---
+++ Sources/NZSSystemExtensions/Shared/SFContentBlockerManager.swift
@@ -0,0 +1,38 @@
+//
+//  SFContentBlockerManager.swift
+//  ScriptAway
+//
+//  Created by Kyle Nazario on 2/28/21.
+//
+
+import Combine
+import SafariServices
+
+@available(macCatalyst 13.4, *)
+public extension SFContentBlockerManager {
+    static func reloadContentBlocker(withIdentifier identifier: String) -> AnyPublisher<Void, Error> {
+        let reloadTracker = PassthroughSubject<Void, Error>()
+        SFContentBlockerManager.reloadContentBlocker(withIdentifier: identifier) { error in
+            if let error = error {
+                reloadTracker.send(completion: .failure(error))
+            } else {
+                reloadTracker.send(())
+            }
+        }
+        return reloadTracker.eraseToAnyPublisher()
+    }
+
+    static func getStateOfContentBlocker(withIdentifier identifier: String)
+        -> AnyPublisher<SFContentBlockerState?, Error>
+    {
+        let stateTracker = PassthroughSubject<SFContentBlockerState?, Error>()
+        SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: identifier) { state, error in
+            if let error = error {
+                stateTracker.send(completion: .failure(error))
+            } else {
+                stateTracker.send(state)
+            }
+        }
+        return stateTracker.eraseToAnyPublisher()
+    }
+}
