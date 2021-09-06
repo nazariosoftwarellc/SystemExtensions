821ca00447ff87feab03cce4a591f8bc33dbac1e
kyle-n <kylebnazario@gmail.com>
2021-09-06 16:34:15 -0400

    NSItemProvider

---
+++ Sources/NZSSystemExtensions/Shared/NSItemProvider.swift
@@ -0,0 +1,26 @@
+//
+//  NSItemProvider.swift
+//  NSItemProvider
+//
+//  Created by Kyle Nazario on 9/1/21.
+//
+
+import Foundation
+import Combine
+
+extension NSItemProvider {
+    
+    @available(macOS 10.15, *)
+    func loadItem(forTypeIdentifier identifier: String, options: [AnyHashable : Any]?) -> PassthroughSubject<NSSecureCoding?, Error> {
+        let subject = PassthroughSubject<NSSecureCoding?, Error>()
+        loadItem(forTypeIdentifier: identifier, options: options) { content, error in
+            if let error = error {
+                subject.send(completion: .failure(error))
+                return
+            }
+            subject.send(content)
+            subject.send(completion: .finished)
+        }
+        return subject
+    }
+}
