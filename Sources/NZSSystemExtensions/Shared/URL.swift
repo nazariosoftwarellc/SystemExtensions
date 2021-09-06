7bad656bfbbfaefdddfbf89c5884ac0cea2a2e65
kyle-n <kylebnazario@gmail.com>
2021-09-06 16:57:39 -0400

    URL

--- Sources/NZSSystemExtensions/Shared/URL.swift
+++ Sources/NZSSystemExtensions/Shared/URL.swift
@@ -17,4 +17,13 @@ public extension URL {
 
         return fileContainer.appendingPathComponent("\(databaseName).sqlite")
     }
+
+    var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+              let queryItems = components.queryItems
+        else { return nil }
+        return queryItems.reduce(into: [String: String]()) { result, item in
+            result[item.name] = item.value
+        }
+    }
 }
