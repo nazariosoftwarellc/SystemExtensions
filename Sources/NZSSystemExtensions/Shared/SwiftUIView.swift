31bfdce3cec4dd7f24ebd3f0520a4d13de830887
Kyle Nazario <nazariosoftwarellc@gmail.com>
2022-06-28 13:23:56 -0400

    EmptyView

---
+++ Sources/NZSSystemExtensions/Shared/SwiftUIView.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by tester on 6/28/22.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 0, height: 0)
+    }
+}
