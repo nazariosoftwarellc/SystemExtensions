a0f40a5595e5ea47c87546ba0e1d78d22862770f
Kyle Nazario <nazariosoftwarellc@gmail.com>
2024-12-01 13:18:42 -0700

    Layout for app list

--- Sources/NZSSystemExtensions/Shared/NZSAppList.swift
+++ Sources/NZSSystemExtensions/Shared/NZSAppList.swift
@@ -5,7 +5,16 @@
 //  Created by Kyle Nazario on 12/1/24.
 //
 
-fileprivate final class NZSAppListViewModel: ObservableObject {
+import SwiftUI
+#if os(macOS)
+import AppKit
+typealias SystemFramework = AppKit
+#else
+import UIKit
+typealias SystemFramework = UIKit
+#endif
+
+public final class NZSAppListViewModel: ObservableObject {
     struct NZSAppLink: Identifiable {
         let name: String
         let href: String
@@ -33,5 +42,33 @@ fileprivate final class NZSAppListViewModel: ObservableObject {
         let (data, _) = try await URLSession.shared.data(from: appJSONURL)
         let decoder = JSONDecoder()
         return try decoder.decode([NZSAppLink].self, from: data)
+    }
+}
+
+public struct NZSAppList: View {
+    @StateObject private var viewModel = NZSAppListViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("More great apps from Nazario Software")
+                .font(.title)
+                .padding(.bottom)
+            List(viewModel.appLinks) { appLink in
+                AppLinkItem(appLink: appLink)
+            }
+        }
+    }
+    
+    struct AppLinkItem: View {
+        let appLink: NZSAppListViewModel.NZSAppLink
+        
+        var body: some View {
+            Button(action: {
+                guard let url = URL(string: appLink.href) else { return }
+                SystemFramework.shared.open(url)
+            }) {
+                Text(appLink.name)
+            }
+        }
     }
 }
